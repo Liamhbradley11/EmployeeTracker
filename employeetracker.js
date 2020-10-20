867736dde04cf1ffb1436d22ddd2784ab04fd60b
Liam Bradley <liamhbradley11@gmail.com>
2020-10-20 07:37:47 -0400

    inital prompts working

--- employeetracker.js
+++ employeetracker.js
@@ -5,23 +5,52 @@ const cTable = require('console.table');
 
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "employeetracker_db"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employeetracker_db"
 });
 
-connection.connect(function(err) {
+
+connection.connect((err) => {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
-  });
-  
-  function afterConnection() {
-    connection.query("SELECT * FROM songs", function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      connection.end();
-    });
-  }
+    init();
+});
+
+function init() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "begin",
+            message: "Would you like to do?",
+            choices: ["View Employees", "View Employees by Department", "Add Employee", "Remove Employee", "Update Employee", "Add Another Role", "Exit"]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View Employees":
+                    joinedTables();
+                    break;
+
+                case "View Employees by Department":
+                    viewDepartment();
+                    break;
+
+                case "Add Employee":
+                    addEmployee();
+                    break;
+
+                case "Remove Employee":
+                    removeRole();
+                    break;
+
+                case "Update Employee":
+                    deleteEmployee();
+                    break;
+
+                case "Exit":
+                    process.exit();
+                    break;
+            }
+        });
+};
